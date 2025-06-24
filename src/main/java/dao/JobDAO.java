4b728e16d2945943aa958c6cb536c406072f3a80
lmaolma <nguyenphuoctruong24092006@gmail.com>
2025-06-25 01:59:31 +0700

    Fix postJob in JobDAO using currentSession and Spring transaction

--- src/main/java/dao/JobDAO.java
+++ src/main/java/dao/JobDAO.java
@@ -1,55 +1,51 @@
 package dao;
 
 import model.Job;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
+@Transactional
 public class JobDAO {
 
     @Autowired
     private SessionFactory sessionFactory;
 
-    /* Thêm Job */
-    public boolean postJob(Job job) {
-        Transaction tx = null;
-        try (Session session = sessionFactory.openSession()) {
-            tx = session.beginTransaction();
-            session.save(job);
-            tx.commit();
-            return true;
-        } catch (Exception ex) {
-            if (tx != null) tx.rollback();
-            ex.printStackTrace();
-        }
-        return false;
-    }
+    /* ===============  C R U D  =============== */
 
-    /* Lấy tất cả Job */
-    public List<Job> getAllJobs() {
-        try (Session session = sessionFactory.openSession()) {
-            return session.createQuery("FROM Job", Job.class).list();
-        }
-    }
-
-    /* Tìm kiếm */
-    public List<Job> searchJobs(String kw) {
-        try (Session session = sessionFactory.openSession()) {
-            return session.createQuery(
-                            "FROM Job WHERE title LIKE :kw OR company LIKE :kw OR description LIKE :kw",
-                            Job.class)
-                    .setParameter("kw", "%" + kw + "%")
-                    .list();
-        }
-    }
-
-    /* Lưu từ CSV */
     public void save(Job job) {
-        postJob(job);   // hoặc tách logic nếu cần
+        sessionFactory.getCurrentSession().save(job);
+    }
+
+    public boolean postJob(Job job) {
+        try {
+            sessionFactory.getCurrentSession().save(job);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public List<Job> getAll() {
+        return sessionFactory.getCurrentSession()
+                .createQuery("FROM Job", Job.class)
+                .list();
+    }
+
+    public List<Job> search(String kw) {
+        String hql = "FROM Job " +
+                "WHERE title LIKE :kw " +
+                "OR company LIKE :kw " +
+                "OR location LIKE :kw " +
+                "OR description LIKE :kw";
+        return sessionFactory.getCurrentSession()
+                .createQuery(hql, Job.class)
+                .setParameter("kw", "%" + kw + "%")
+                .list();
     }
 }
